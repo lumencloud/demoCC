2caf81e8b5851ad781a1e2f647e914c6c867a615
cc.tg00698@partner.sk.com <cc.tg00698@partner.sk.com>
2025-07-14 04:19:40 +0000

    5555

--- BIX-portal-new/scripts/reset-deploy-config.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-fs.renameSync('./package_origin.json', './package.json');
-fs.renameSync('./approuter/package_origin.json', './approuter/package.json');
-fs.renameSync('./approuter/xs-app_origin.json', './approuter/xs-app.json');
-fs.renameSync('./mta_origin.yaml', './mta.yaml');
--- BIX-portal/db/cds/common/card_target.cds
+++
@@ -1,8 +0,0 @@
-using common.card as card from './card';
-
-namespace common;
-
-entity card_target {
-  key card      : Association to card;
-  key targetSeq : String(50);
-}
